e3c813b6fa44bc22eed4338090e4eee43343523e
lesoura <marvirtlester.mb@gmail.com>
2025-12-05 16:08:12 +0800

    PC Adjustment

--- app/introduction.tsx
+++ app/introduction.tsx
@@ -17,7 +17,7 @@ export default function Introduction() {
           "
           initial={{ opacity: 0, y: -50 }}
           whileInView={{ opacity: 1, y: 0 }}
-          viewport={{ once: false, amount: 0.7 }}
+          viewport={{ once: false, amount: 0.6 }}
           transition={{ duration: 0.8, ease: "easeOut" }}
         >
           <img
